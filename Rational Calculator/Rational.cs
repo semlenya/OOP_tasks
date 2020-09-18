6ba0dc81107469730cd3bef2cf76e02a6f106a77
semlenya <lesyakyshat@gmail.com>
2020-09-18 22:12:00 +0300

    Add Test Project

--- Rational Calculator/Rational.cs
+++ Rational Calculator/Rational.cs
@@ -23,18 +23,70 @@ namespace Rational_Calculator
             znam = zn;
         }
 
-        public static Rational Sum(Rational d2)
+        public static Rational Sum(Rational d1, Rational d2)
         {
-            HMACRIPEMD160 ;
             Rational d3 = new Rational();
             d3.chis = d1.chis * d2.znam + d2.chis * d1.znam;
             d3.znam = d1.znam * d2.znam;
+            if (d3.chis == d3.znam)
+            {
+                d3.chis = 1;
+                d3.znam = 1;
+            }
             return d3;
         }
 
+        public static Rational Raz(Rational d1, Rational d2)
+        {
+            Rational d3 = new Rational();
+            d3.chis = d1.chis * d2.znam - d2.chis * d1.znam;
+            d3.znam = d1.znam * d2.znam;
+            if (d3.chis == d3.znam)
+            {
+                d3.chis = 1;
+                d3.znam = 1;
+            }
+            return d3;
+        }
+        public static Rational Umn(Rational d1, Rational d2)
+        {
+            Rational d3 = new Rational();
+            d3.chis = d1.chis * d2.chis;
+            d3.znam = d1.znam * d2.znam;
+            if (d3.chis == d3.znam)
+            {
+                d3.chis = 1;
+                d3.znam = 1;
+            }
+            return d3;
+        }
+        public static Rational Del(Rational d1, Rational d2)
+        {
+            Rational d3 = new Rational();
+            d3.chis = d1.chis * d2.znam;
+            d3.znam = d1.znam * d2.chis;
+            if (d3.chis == d3.znam)
+            {
+                d3.chis = 1;
+                d3.znam = 1;
+            }
+            return d3;
+        }
         public static Rational operator +(Rational d1, Rational d2)
         {
-            return d1.Sum(d2);
+            return Sum(d1, d2);
+        }
+        public static Rational operator -(Rational d1, Rational d2)
+        {
+            return Raz(d1, d2);
+        }
+        public static Rational operator *(Rational d1, Rational d2)
+        {
+            return Umn(d1, d2);
+        }
+        public static Rational operator /(Rational d1, Rational d2)
+        {
+            return Del(d1, d2);
         }
     }
 }
