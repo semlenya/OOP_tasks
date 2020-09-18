4c00c6dcc1a003d680292ebb37f558c3cda8b21e
semlenya <lesyakyshat@gmail.com>
2020-09-18 18:31:54 +0300

    Add Test Project

--- Rational Calculator/Form1.cs
+++ Rational Calculator/Form1.cs
@@ -16,10 +16,27 @@ namespace Rational_Calculator
         {
             InitializeComponent();
         }
-
+        
         private void Addition_Click(object sender, EventArgs e)
         {
+            Rational dr1 = new Rational();
+            Rational dr2 = new Rational();
+            if (textBox1.Text == null || textBox2.Text == null || textBox3.Text == null || textBox4.Text == null || textBox2.Text == "0" || textBox4.Text == "0")
+            {
+                MessageBox.Show("Error");
+            }
+            else
+            {
+                try
+                {
+                    Rational dr3 = dr1 + dr2;
 
+                }
+                catch
+                {
+
+                }
+            }
         }
 
         private void Subtraction_Click(object sender, EventArgs e)
--- Rational Calculator/Rational.cs
+++ Rational Calculator/Rational.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,31 @@ namespace Rational_Calculator
 {
     class Rational
     {
+        public int chis;
+        public int znam;
+
+        public Rational()
+        {
+
+        }
+        public Rational( int ch, int zn)
+        {
+            chis = ch;
+            znam = zn;
+        }
+
+        public static Rational Sum(Rational d2)
+        {
+            HMACRIPEMD160 ;
+            Rational d3 = new Rational();
+            d3.chis = d1.chis * d2.znam + d2.chis * d1.znam;
+            d3.znam = d1.znam * d2.znam;
+            return d3;
+        }
+
+        public static Rational operator +(Rational d1, Rational d2)
+        {
+            return d1.Sum(d2);
+        }
     }
 }
