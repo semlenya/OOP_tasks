87b2722d353f19dced0d407c327458d5479fb185
semlenya <lesyakyshat@gmail.com>
2020-09-18 17:42:59 +0300

    Add Test Project

---
+++ Rational Calculator/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rational_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Addition_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Subtraction_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Miltiplication_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Division_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
