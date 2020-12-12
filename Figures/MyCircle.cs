275926e2d16db22f2ee7507e258ec747a5be200b
Алеся Семенова <lesyakyshat@gmail.com>
2020-12-12 11:26:28 +0300

    Я на семинарах показывала, теперь рисуется поезд со всеми видами вагонов

---
+++ Figures/MyCircle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Figures
+{
+    class MyCircle : MyFigure
+    {
+        public int radius;
+
+        public MyCircle()
+        { }
+
+        public MyCircle(int X, int Y, int Radius)
+        {
+            x = X;
+            y = Y;
+            radius = Radius;
+        }
+        public override void Draw(Pen pen, Brush brush, Graphics graph)
+        {
+            //graph.DrawEllipse(pen, x - radius, y - radius,  2 * radius, 2 * radius);
+            Rectangle ellipse = new Rectangle(x - radius, y - radius, 2 * radius, 2 * radius);
+            graph.FillEllipse(brush, ellipse);
+        }
+    }
+}
---
+++ Figures/MyWagon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Figures
+{
+    class MyWagon : MyRectangle
+    {
+        public int radius;
+        MyCircle wheel1 = new MyCircle();
+        MyCircle wheel2 = new MyCircle();
+        public MyWagon()
+        { }
+        
+        public MyWagon(int X, int Y, int Weight, int Height)
+        {
+            x = X;
+            y = Y;
+            weight = Weight;
+            height = Height;
+            radius = weight / 5;
+        }
+        public override void Draw(Pen pen, Brush brush, Graphics graph)
+        {
+            base.Draw(pen, brush, graph);
+            wheel1 = new MyCircle(x + radius, y + height + radius, radius);
+            wheel2 = new MyCircle(x + weight - 2 * radius + radius, y + height + radius, radius);
+            wheel1.Draw(pen, brush, graph);
+            wheel2.Draw(pen, brush, graph);
+        }
+    }
+}
